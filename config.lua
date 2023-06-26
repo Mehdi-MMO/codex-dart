1e4eeedeeb455a80e690917a828e4aecf214c1c7
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-06-26 11:42:44 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,10 @@ Config = {}
 -- Dart settings
 Config.DartCooldown = 300 -- Cooldown in seconds for the /removedart command
 Config.DartTimer = 10 -- Timer in minutes for the dart attachment
+Config.UseCommand = false -- Set to true to use the command, false to use the keybind
+Config.CommandName = 'firedart' -- Command name to fire the dart
+Config.Keybind = 'G' -- Keybind to fire the dart
+
 
 -- Blip settings
 Config.BlipColors = {
