1400a04eb0b512a3a968cbcd33516d133d1f3a53
Mehdi <57235093+Mehdi-MMO@users.noreply.github.com>
2023-07-09 14:22:03 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,14 +3,18 @@ Config = {}
 -- Dart settings
 Config.DartCooldown = 300 -- Cooldown in seconds for the /removedart command
 Config.DartTimer = 10 -- Timer in minutes for the dart attachment
-Config.UseCommand = false -- Set to true to use the command, false to use the keybind
+Config.UseKeybind = false -- Set to true to use the keybind, false to use the command only
 Config.CommandName = 'firedart' -- Command name to fire the dart
 Config.Keybind = 'G' -- Keybind to fire the dart
-
+Config.useNDCore = false
+Config.PoliceJobs = {'LSPD', 'BCSO'} -- For NDCore
 
 -- Blip settings
-Config.BlipColors = {
-    Blue = 26,
-    Orange = 47,
-    Red = 1
+Config.Blip = {
+    Color = {
+        LongDuration = 26, -- Blip color for durations over 20% of the time left
+        MediumDuration = 47, -- Blip color for durations between 10% and 20% of the time left
+        ShortDuration = 1 -- Blip color for durations less than 10% of the time left
+    },
+    Sprite = 794
 }
